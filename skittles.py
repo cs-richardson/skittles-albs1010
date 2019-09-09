1949fcc45ee70a9388d62cc1d1fbb755e3660719
Albert wang <awang21@ssis.edu.vn>
2019-09-09 16:22:13 +0700

    Update skittles.py

--- skittles.py
+++ skittles.py
@@ -7,22 +7,22 @@ the user with certain hints until the user guesses right.
 import random
 random_number = random.randint(0,1023)
 
-ask = int(input("O hai! I'm thinking of a number between 0 and 1023. What is it?\n"))
+guess = int(input("O hai! I'm thinking of a number between 0 and 1023. What is it?\n"))
 
-while ask != random_number:
+while guess != random_number:
     
 #Hints that helps the user guess more accurately
     
-    if ask > random_number and ask < 1023:
+    if guess > random_number and guess < 1023:
         print("Nope! There are fewer Skittles than that. Guess again.")
         
-    elif ask < random_number and ask > -1:
+    elif guess < random_number and guess > -1:
         print("Nope! There are way more Skittles than that. Guess again.")
 
     else:
         print("Nope! Don't be difficult. Guess again.")
     
-    ask = int(input(""))
+    guess = int(input(""))
     
 
 print("That's right! Nom nom nom")
